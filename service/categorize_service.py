2668b61851c3747d81ef72db6b460d53707c571e
taejin1221 <taejin7824@gmail.com>
2024-10-24 22:20:46 +0900

    Feat: 하위 카테고리 분류 서비스 추가
    #7

--- service/categorize_service.py
+++ service/categorize_service.py
@@ -13,6 +13,9 @@ class CategorizeService:
         with open("prompt/main_category", "r") as f:
             self.main_category_prompt = f.read()
 
+        with open("prompt/sub_category", "r") as f:
+            self.sub_category_prompt = f.read()
+
     def categorize_main(self, content: str) -> str:
         """
         메인 카테고리를 분류
@@ -22,3 +25,19 @@ class CategorizeService:
         category = json.loads(self.gpt_model.generate_response(self.main_category_prompt, content))
 
         return category["main_categories"][0]
+
+    def categorize_sub(self, content: str, main_category: str, all_sub_categories: set[str]) -> list[str]:
+        """
+        하위 카테고리를 분류
+        :param content: 분류하려는 텍스트
+        :param main_category: 텍스트의 주요 카테고리
+        :param all_sub_categories: 텍스트의 하위 카테고리 목록
+        :return:
+        """
+
+        user_message = f"주요 카테고리: {main_category}, 텍스트: {content}"
+        system_message = f"{self.sub_category_prompt}[{', '.join(all_sub_categories)}]"
+
+        sub_categories = json.loads(self.gpt_model.generate_response(system_message, user_message))
+
+        return sub_categories["sub_categories"]
