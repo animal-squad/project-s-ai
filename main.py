4c15a049ca52650105ff87c7e21a6730a2aedef5
taejin1221 <taejin7824@gmail.com>
2024-10-24 23:50:48 +0900

    Feat: POST /ai/categorize/sub API 작성
    - 하위 카테고리 분류 API 추가
    
    #7

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from entity.request_dto import CategorizeMainRequest
-from entity.response_dto import CategorizeMainResponse
+from entity.content import ContentSubCategory
+from entity.request_dto import CategorizeMainRequest, CategorizeSubRequest
+from entity.response_dto import CategorizeMainResponse, CategorizeSubResponse
 from model.gpt_model import GPTModel
 from service.categorize_service import CategorizeService
 
@@ -17,3 +18,29 @@ async def classify_main(req: CategorizeMainRequest):
     """
     category = categorize_service.categorize_main(req.content)
     return CategorizeMainResponse(category=category)
+
+
+@app.post("/ai/categorize/sub")
+async def classify_sub(req: CategorizeSubRequest):
+    """
+    하위 카테고리 분류 엔드포인트
+    """
+    # 주요 카테고리가 가지고 있는 모든 하위 카테고리 리스트
+    sub_categories_of_main = { key: val for sub in req.sub_categories for key, val in sub.items() }
+
+    results = []
+    for content_info in req.data:
+        content_id, content_type, main_category, content = content_info.dict().values()
+        sub_categories = categorize_service.categorize_sub(
+            content,
+            main_category,
+            all_sub_categories=sub_categories_of_main[main_category]
+        )
+
+        results.append(ContentSubCategory(
+            content_id=content_id,
+            type=content_type,
+            sub_categories=sub_categories
+        ))
+
+    return CategorizeSubResponse(data=results)
