28bb7708911be392bfee3d9de4071b6bc556e419
taejin1221 <taejin7824@gmail.com>
2024-11-09 12:00:03 +0900

    Fix: LinkInfo에 nullable 추가
    # 15

--- entity/link_info.py
+++ entity/link_info.py
@@ -3,9 +3,9 @@ from pydantic import BaseModel
 class LinkInfo(BaseModel):
     linkId: int
     link: str
-    content: str
+    content: str | None
 
 class LinkWithTags(BaseModel):
     linkId: int
-    title: str
+    title: str | None
     tags: list[str]
