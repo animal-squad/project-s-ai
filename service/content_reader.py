17487442a9740c439c929592846b951e2e73bf9b
taejin1221 <taejin7824@gmail.com>
2024-11-09 12:03:17 +0900

    Feat: HTML의 본문정보 추출하는 기능 추가
    #15

--- service/content_reader.py
+++ service/content_reader.py
@@ -3,14 +3,16 @@ import os
 
 import requests
 
+from service.content_extractor import ContentExtractor
 from service.crawlability_checker import CrawlabilityChecker
 
 
 class ContentReader:
-    def __init__(self, crawlability_checker: CrawlabilityChecker):
+    def __init__(self, crawlability_checker: CrawlabilityChecker, content_extractor: ContentExtractor):
         self.GOOGLE_SEARCH_API_KEY = os.getenv("GOOGLE_SEARCH_API_KEY")
         self.GOOGLE_SEARCH_CX = os.getenv("GOOGLE_SEARCH_CX")
         self.crawlability_checker = crawlability_checker
+        self.content_extractor = content_extractor
 
     def fetch_html_content(self, url: str) -> str:
         """
@@ -40,15 +42,32 @@ class ContentReader:
         except KeyError:
             return None
 
-    def read_content(self, url: str) -> str | None:
+    def extract(self, url: str, content: str) -> dict[str, str] | None:
+        if url.find("youtube.com/watch") != -1:
+            return self.content_extractor.extract_youtube(content)
+        elif url.find("velog.io/@") != -1:
+            return self.content_extractor.extract_velog(content)
+        elif url.find("okky.kr/questions") != -1 or url.find("okky.kr/articles") != -1:
+            return self.content_extractor.extract_okky(content)
+        elif url.find("tistory.com") != -1:
+            return self.content_extractor.extract_tistory(content)
+        else:
+            return self.content_extractor.extract_generic(content)
+
+    def read_content(self, url: str, content: str | None) -> dict[str, str] | None:
         """
         URL의 컨텐츠 정보를 반환. 만약 정보를 읽을 수 없다면 None 반환
         :param url: 내용을 알고 싶은 URL
+        :param content: URL의 정보
         :return:
             읽을 수 있는 정보 반환
             읽을 수 없는 정보라면 None 반환
         """
-        if self.crawlability_checker.can_crawl(url):
-            return self.fetch_html_content(url)
+        if not content:
+            if self.crawlability_checker.can_crawl(url):
+                content = self.fetch_html_content(url)
+
+        if content:
+            return self.extract(url, content)
         else:
             return self.perform_google_search(url)
